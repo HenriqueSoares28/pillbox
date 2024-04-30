0ff6ea2dd25ca992e05e926877cda81759722c72
Lloures <louresluiza6776@gmail.com>
2024-04-30 18:53:48 -0300

    Principal

--- lib/navbar.dart
+++ lib/navbar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class Navbar extends StatelessWidget {
   const Navbar({super.key});
-
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
