9ce89626b0482e9787f2f569849a7f09fa9f1158
Henrique França Carvalho Soares <69248621+HenriqueSoares28@users.noreply.github.com>
2024-05-02 21:36:14 -0300

    feat: Update visualizarRemedio widget with new design
    The code changes in the visualizarRemedio.dart file update the widget's design by changing the background color to black and updating the text colors to white. This commit message follows the convention of starting with a type (feat for feature) and providing a concise and clear description of the changes made.

--- lib/visualizarRemedio.dart
+++ lib/visualizarRemedio.dart
@@ -5,36 +5,52 @@ class OvalWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 300,
-      height: 500,
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(245, 255, 249, 226),
-        borderRadius: BorderRadius.circular(50),
-      ),
-      child: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            '1',
-            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10),
-          Text(
-            'NOME DO REMÉDIO',
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10),
-          Text(
-            'HORÁRIO:',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10),
-          Text(
-            'DIAS:',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-        ],
-      ),
+        width: 300,
+        height: 500,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 11, 11, 11),
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '1',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'NOME DO REMÉDIO',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'HORÁRIO:',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'DIAS:',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
     );
   }
 }
